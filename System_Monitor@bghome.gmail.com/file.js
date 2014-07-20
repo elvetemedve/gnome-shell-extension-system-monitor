fcd6f3f5ec0bc633cdc5c1b396fcdc395ea6d29b
Géza Búza <bghome@gmail.com>
2014-07-20 22:33:25 +0200

    Fix: too many open files error
    After a while Gnome gives a "Gio.IOErrorEnum: Too many open files" error during a file operation.
    This fix closes the GFileEnumerator input stream when it no longer needed.

--- System_Monitor@bghome.gmail.com/file.js
+++ System_Monitor@bghome.gmail.com/file.js
@@ -10,9 +10,9 @@ const File = new Lang.Class({
 
 	getContents: function() {
 		let file = Gio.File.new_for_path(this._filename);
-		let content = '' + file.load_contents(null)[1];
-		file.unref();
-		return content;
+		let contents = file.load_contents(null);
+		let result = '' + contents[1];
+		return result;
 	},
 
 	list: function() {
@@ -22,7 +22,7 @@ const File = new Lang.Class({
 		while ((file_info = list.next_file(null)) != null) {
 			results.push(file_info.get_attribute_as_string(Gio.FILE_ATTRIBUTE_STANDARD_NAME));
 		}
-		file.unref();
+		list.close(null);
 		return results;
 	}
 });
