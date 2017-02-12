29f3e3c1b35f9f998ff051c4400a21c64783ea5d
Géza Búza <bghome@gmail.com>
2017-02-12 16:18:16 +0100

    Add StopWatch class
    To be able to test the performance of meters, the StopWatch class can
    measure the time spent with a task, showing performance bottlenecks.
    
    Usage:
    let stopwatch = new Util.StopWatch();
    stopwatch.start();
    // here goes the code to be measured
    stopwatch.stop();
    stopwatch.printElapsed('Time spent with task:');

--- System_Monitor@bghome.gmail.com/util.js
+++ System_Monitor@bghome.gmail.com/util.js
@@ -169,3 +169,42 @@ let Swap = new Lang.Class({
         return false;
     }
 });
+
+let StopWatch = function () {
+    this.startTime = 0;
+    this.stopTime = 0;
+    this.running = false;
+    this.performance = !!window.performance;
+};
+
+StopWatch.prototype.currentTime = function () {
+    return this.performance ? window.performance.now() : new Date().getTime();
+};
+
+StopWatch.prototype.start = function () {
+    this.startTime = this.currentTime();
+    this.running = true;
+};
+
+StopWatch.prototype.stop = function () {
+    this.stopTime = this.currentTime();
+    this.running = false;
+};
+
+StopWatch.prototype.getElapsedMilliseconds = function () {
+    if (this.running) {
+        this.stopTime = this.currentTime();
+    }
+
+    return this.stopTime - this.startTime;
+};
+
+StopWatch.prototype.getElapsedSeconds = function () {
+    return this.getElapsedMilliseconds() / 1000;
+};
+
+StopWatch.prototype.printElapsed = function (name) {
+    var currentName = name || 'Elapsed: ';
+
+    window.log(currentName + '[' + this.getElapsedMilliseconds() + 'ms]' + '[' + this.getElapsedSeconds() + 's]');
+};
